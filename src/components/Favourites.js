e99d5ae1206b3b2703b17f24c7bef6765f4e239c
shreyask1416 <shreya.sk@robosoftin.com>
2020-12-29 01:15:34 +0530

    evalup

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,30 +1,33 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import '../Style/Weather.css'
 function Favourites({ info, setinfo }) {
-    const [step, setstep] = useState();
-    const DeleteInfo = () => {
-        if (window.confirm("Do you want to delete this from favourites?")) {
-            let newList = [...info];
-            newList.splice(step, 1);
-            setinfo(newList);
-            setstep(step - 1);
-
-
+    const [s, setS] = useState();
+    const removeall = () => {
+        if (window.confirm("Do you want to delete these from favourites?")) {
+            let del = [...info];
+            del.splice(s);
+            setinfo(del);
+            setS(s - 1);
         }
     }
     return (
         <div >
+            <div>
+                <button onClick={removeall} className="remove">Remove all</button>
+            </div>
             {info.map((menuItem) => {
                 const { name, icon, tempC, desc } = menuItem;
+
                 return (
-                    <div className="fav">
+                    < div className="fav">
                         <h1>{name} </h1>
                         <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} />
                         <p>{tempC}</p>
                         <p>{desc}</p>
                         <div>
-                            <button onClick={DeleteInfo} className="material-icons">favorite</button>
+                            <button  className="material-icons">favorite</button>
                         </div>
+                        <div />
                     </div>
                 )
             })}
